a06c42c7393351f75f1cd4b31803c608bc8f6dcd
Reis <80530563+JoaoReis153@users.noreply.github.com>
2026-04-12 18:24:47 +0100

    test: add -I method verification

--- galleryLog/src/test/java/cli/LogScenarioStateTest.java
+++ galleryLog/src/test/java/cli/LogScenarioStateTest.java
@@ -69,4 +69,39 @@ class LogScenarioStateTest {
 
         assertEquals("1,2,3,1\n", output.toString(StandardCharsets.UTF_8));
     }
+
+    @Test
+    void intersectionReturnsOnlyRoomsWithRealOverlap() throws Exception {
+        Path logsDir = tempDir.resolve("logs");
+        Files.createDirectories(logsDir);
+        Path logPath = logsDir.resolve("logI");
+        String log = logPath.toString();
+
+        LogAppend append = new LogAppend();
+        append.handle("-T 1 -K secret -A -E Fred " + log);
+        append.handle("-T 2 -K secret -A -G Jill " + log);
+
+        // Room 1 has overlap: Fred [3,7), Jill [4,6)
+        append.handle("-T 3 -K secret -A -E Fred -R 1 " + log);
+        append.handle("-T 4 -K secret -A -G Jill -R 1 " + log);
+        append.handle("-T 6 -K secret -L -G Jill -R 1 " + log);
+        append.handle("-T 7 -K secret -L -E Fred -R 1 " + log);
+
+        // Room 2 has no overlap: Fred [8,9), Jill [10,11)
+        append.handle("-T 8 -K secret -A -E Fred -R 2 " + log);
+        append.handle("-T 9 -K secret -L -E Fred -R 2 " + log);
+        append.handle("-T 10 -K secret -A -G Jill -R 2 " + log);
+        append.handle("-T 11 -K secret -L -G Jill -R 2 " + log);
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        try {
+            System.setOut(new PrintStream(output, true, StandardCharsets.UTF_8));
+            new LogRead().handle("-K secret -I -E Fred -G Jill " + log);
+        } finally {
+            System.setOut(originalOut);
+        }
+
+        assertEquals("1\n", output.toString(StandardCharsets.UTF_8));
+    }
 }
